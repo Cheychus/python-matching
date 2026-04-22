ec7491915130b2e7ba98c5e7407871729c538617
Alexander Cichacki <a.cichacki@ostfalia.de>
2026-04-22 11:30:26 +0200

    fixed overwrite with benchmark

--- src/embeddings/create_embeddings.py
+++ src/embeddings/create_embeddings.py
@@ -11,7 +11,7 @@ from config import (
 from src.model.embedding_model import get_model
 
 
-def create_embeddings():
+def create_embeddings(save=False):
     model = get_model()
 
     idx = 1
@@ -22,26 +22,24 @@ def create_embeddings():
             terms = json.load(f)
 
         if config.EMBEDDING_LIMIT is not None:
-            terms = terms[:config.EMBEDDING_LIMIT]
+            terms = terms[: config.EMBEDDING_LIMIT]
         for t in terms:
             if config.SELECTED_MODEL == "nomic-ai/nomic-embed-text-v1.5":
-                # print("[EMBEDDING]: Using search_document: as a prompt")
                 embedding_inputs.append("search_document: " + t["embedding_input"])
             elif config.SELECTED_MODEL == "intfloat/multilingual-e5-large":
-                # print("[EMBEDDING]: Using passage: as a prompt")
                 embedding_inputs.append("passage: " + t["embedding_input"])
             else:
-                #  print("[EMBEDDING]: Using no prompt")
                 embedding_inputs.append(t["embedding_input"])
         print(f"[EMBEDDING]: {ontology} {idx}/{len(ONTOLOGIES_LIST)}")
 
         embeddings = model.encode(
             embedding_inputs, batch_size=32, show_progress_bar=False
         )
-        np.save(
-            str(EMBEDDINGS_DIR / config.SELECTED_MODEL / f"{ontology}_vectors.npy"),
-            embeddings,
-        )
+        if save:
+            np.save(
+                str(EMBEDDINGS_DIR / config.SELECTED_MODEL / f"{ontology}_vectors.npy"),
+                embeddings,
+            )
         idx += 1
 
     total = time.perf_counter() - start_total
